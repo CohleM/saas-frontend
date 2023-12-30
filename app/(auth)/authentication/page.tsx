ac210c0d542ee923598793d74bb1dc60871f5160
CohleM <manish981194@gmail.com>
2023-12-30 21:42:59 +0545

    magic link connect with backend

--- app/(auth)/authentication/page.tsx
+++ app/(auth)/authentication/page.tsx
@@ -1,90 +1,64 @@
-"use client"
+'use client'
+// MagicLinkPage.tsx
 
-import { Icons } from "@/components/icons"
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { useEffect } from 'react';
+import { redirect, useSearchParams } from 'next/navigation';
 
-export default function CreateAccount() {
-  return (
-    // <div className="flex justify-center items-center mt-24">
-    // <Card className="w-96">
-    //   <CardHeader className="space-y-1">
-    //     <CardTitle className="text-2xl">Create an account</CardTitle>
-    //     <CardDescription>
-    //       Enter your email below to create your account
-    //     </CardDescription>
-    //   </CardHeader>
-    //   <CardContent className="grid gap-4">
-    //     <div className="">
-          
-    //       <Button variant="outline" className="w-full">
-    //         <Icons.google className="mr-2 h-4 w-4" />
-    //         Continue with Google
-    //       </Button>
-    //     </div>
-    //     <div className="relative">
-    //       <div className="absolute inset-0 flex items-center">
-    //         <span className="w-full border-t" />
-    //       </div>
-    //       <div className="relative flex justify-center text-xs uppercase">
-    //         <span className="bg-background px-2 text-muted-foreground">
-    //           Or continue with
-    //         </span>
-    //       </div>
-    //     </div>
-    //     <div className="grid gap-2">
-    //       <Label htmlFor="email">Email</Label>
-    //       <Input id="email" type="email" placeholder="m@example.com" />
-    //     </div>
-    //     <div className="grid gap-2">
-    //       <Label htmlFor="password">Password</Label>
-    //       <Input id="password" type="password" />
-    //     </div>
-    //   </CardContent>
-    //   <CardFooter>
-    //     <Button className="w-full">Create account</Button>
-    //   </CardFooter>
-    // </Card>
-    // </div>
+const MagicLinkPage: React.FC = () => {
+  const searchParams = useSearchParams()
+
+  const token = searchParams.get('token') 
+  console.log(token)
+
+  useEffect(() => {
+
+    // Check if the token is present in the URL
+    if (token) {
+      // Make a request to the backend to verify the token
+      // fetch(`http://127.0.0.1:8000/token/${token}`)
+      //   .then((response) => response.json())
+      //   .then((data) => {
+      //     // Handle the verification response
+      //     console.log(data.message);
+      //     // Redirect the user or show an error message
+      //   })
+      //   .catch((error) => {
+      //     console.error('Error verifying magic link:', error);
+      //     // Show an error message to the user
+      //   });
+
+      const veryToken = async () => {
+
+        try {
+          const response = await fetch(`http://127.0.0.1:8000/token/${token}`)
+
+          if (response.ok) {
+            const output = await response.json()
+            // console.log(output)
+            if (output['isValid'] == 'true') {
+                console.log(output['message'])
+                redirect('http://localhost:3000/editor')
+            } 
+            else{
+              console.log(output['message'])
+            }
+          }
+        }
+
+        catch (error) {
+          console.error(error)
+        }
+      }
+
+      veryToken()
+    }
 
 
-    <div className="flex justify-center items-center mt-24">
-    <Card className="w-96">
-      <CardHeader className="flex flex-col space-y-1 justify-center items-center">
-        <CardTitle className="text-2xl">Get Started</CardTitle>
-        <CardDescription>
-        Create your account
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="">
-          
-          <Button variant="default" className="w-full">
-            <Icons.google className="mr-2 h-4 w-4" />
-            Continue with Google
-          </Button>
-          
+  }, []);
 
-          <div className="flex space-x-3 mt-4 justify-evenly">
-            <p>
-              <span className="text-sm text-muted-foreground" >Already have an account?</span> 
-              <span> <a href="/login" className="text-sm font-medium">Login </a> </span> </p>
-           
-          </div>
-        </div>
+  return <div>Verifying magic link...</div>;
+};
 
-      </CardContent>
+export default MagicLinkPage;
 
-    </Card>
-    </div>
-  )
-}
+
